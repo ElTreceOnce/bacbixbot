94d02551a3c6c6730e9b251b2357d104b7a39b68
ElTreceOnce <martinortega1311@gmail.com>
2024-12-20 14:11:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ ADMIN_ID = 1415509092
 
 # Diccionario manual con las claves de los usuarios
 user_keys = {
+    7441756891: "dj",
     8090673440: "rayoazul",
     6929246709: "$$$",
     5649756349: "privado",
