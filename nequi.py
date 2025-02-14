194dab9aed522cf177793ca98386a70d709dfd4f
ElTreceOnce <martinortega1311@gmail.com>
2025-02-14 13:25:37 -0500

    Update nequi.py

--- nequi.py
+++ nequi.py
@@ -62,7 +62,7 @@ async def amount(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     context.user_data['datetime'] = current_datetime
 
     # Aquí se manipula la imagen
-    image_path = os.path.join(IMAGE_DIR, 'nequiclear.png')
+    image_path = os.path.join(IMAGE_DIR, 'nequinuevo.jpg')
     font_path = os.path.join(FONT_DIR, 'Nesquic.ttf')
     img = Image.open(image_path)
     draw = ImageDraw.Draw(img)
